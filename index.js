bafc5d6588bf8aa54fa221d70d98121fb2974ad3
Nykz <62361016+nykz1337@users.noreply.github.com>
2020-07-12 02:17:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client({disableEveryone: true});
-const prefix = "!";
-const token = "NzI5NzQ0NjI1NzY1NTgwODQy.XwNZqg.BgLLJyAC8_EbN7br5zVAjgnSRt0";
+const prefix = "BOT_PREFIX";
+const token = "BOT_TOKEN";
 
 client.on('ready', () => {
 	console.log(`Login as: ${client.user.username}`)
@@ -18,9 +18,8 @@ client.on('message', message => {
 			if(!msg) return message.channel.send(":x: Error! Valid usage: " + `${prefix}` + "dmall <message>");
 			message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
 				m.send(`${msg}`);
-				console.log(`Send to: ${m}`)
 			});
-			let all = message.guild.members.filter(m => m.presence.status === 'online').size
+			let all = message.guild.members.filter(m => m.presence.status !== 'online').size
 			embed = new Discord.RichEmbed()
 			.setTitle("Succes!")
 			.setColor("GREEN")
